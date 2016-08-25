833d287804f2cbad271ffdff8fd1d0001d06d1f6
Alex Lowe <loweman64@outlook.com>
2016-08-25 17:32:28 -0400

    Added 'Lecture 31 Comparison Operators.py'

---
+++ Python/Python Bootcamp/Lecture 31 Comparison Operators.py
@@ -0,0 +1,43 @@
+"""
+Comparison Operators
+"""
+
+print(2 == 2)
+# Output: True
+
+print(1 == 1)
+# Output: True
+
+print(1 == 0)
+# Output: False
+
+# Not Equal
+
+print(2 != 1)
+# Output: True
+
+print(2 != 2)
+# Output: False
+
+# Greater Than / Less Than
+
+print(2 > 1)
+# Output: True
+
+print(2 > 4)
+# Output: False
+
+print(2 < 1)
+# Output: False
+
+print(2 >= 2)
+# Output: True
+
+print(2 >= 1)
+# Output: True
+
+print(2 <= 2)
+# Output: True
+
+print(2 <= 10)
+# Output: True
