2af8fa8201c1c27a36ebd4c326ad021bdd8857ee
Alex Lowe <loweman64@outlook.com>
2016-08-29 01:58:34 -0400

    Added 'Lecture 37 Range.py'

---
+++ Python/Python Bootcamp/Lecture 37 Range.py
@@ -0,0 +1,12 @@
+"""
+Basic statement to print a range
+"""
+print(list(range(0,10)))
+
+
+"""
+Step size with range only will print the even numbers
+"""
+start = 0
+stop = 20
+print(list(range(start,stop,2)))
