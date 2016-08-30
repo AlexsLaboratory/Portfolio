afecc8fdc0483ff7bceb09f2e331d22a9f5a804c
Alex Lowe <loweman64@outlook.com>
2016-08-29 20:03:37 -0400

    Added 'Lecture 41 Methods.py'

---
+++ Python/Python Bootcamp/Lecture 41 Methods.py
@@ -0,0 +1,15 @@
+"""
+append object to end
+"""
+l = [1,2,3,4,5]
+l.append(6)
+print(l)
+# Output: [1, 2, 3, 4, 5, 6]
+
+
+"""
+returns number of occurrences of a value
+"""
+l = [1,2,3,4,5]
+print(l.count(3))
+# Output: 1
