a5b9f8f16962f9b7f2b219da481f5db8bd1ec8b7
Alex Lowe <loweman64@outlook.com>
2016-08-31 01:31:29 -0400

    Made the code look a lot cleaner by tidying it up, fixing little errors, and adding comments to the code

--- Python/Countdown Timer/Version 2/Countdown Timer v3.0 (final).py
+++ Python/Countdown Timer/Version 2/Countdown Timer v3.0 (final).py
@@ -1,235 +1,150 @@
-#!/usr/local/bin/python3.5
-
-import time, datetime
+import time, datetime, sys
 from subprocess import call
 
-Year = 2016
-Month = 1
-Day = 15
-Hour = 13
+"""
+Specify the date that you want to countdown to
+"""
+Year = 2018
+Month = 9
+Day = 30
+Hour = 23
 Minute = 0
 Second = 0
 
-target_date = datetime.datetime(Year, Month, Day, Hour, Minute, Second)
-timedelta_zero = datetime.timedelta(0)
-
-while True:
-	diff = target_date - datetime.datetime.now()
-	diff_str = str(diff)
-
-	if (len(diff_str) >= int(21)):
-		identifacation = int(1)
-		day_str, not_useful, time_str = diff_str.split()
-		hour_str, minute_str, second = time_str.split(":")
-		second_str, not_useful = second.split(".")
-	else:
-		identifacation = int(2)
-		hour_str, minute_str, second = diff_str.split(":")
-		second_str, not_useful = second.split(".")
-
-	if (diff <= timedelta_zero):
+target_date = datetime.datetime(Year, Month, Day, Hour, Minute, Second) # Formats the date entered
+timedelta_zero = datetime.timedelta(0) # Sets time delta to 0:00:00
+while True: # Runs the code with in the while statement infinitely until broken out of
+	time.sleep(1) # Loops every second
+	if sys.platform.startswith('linux') or sys.platform.startswith('darwin'): # Runs code below only if this script is running on Linux or Darwin
+		call('clear', shell=True) # Clears terminal
+	elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'): # Runs code below only if this script is running on Win32 or Cygwin
+		call('cls', shell=True) # Clears terminal
+	diff = target_date - datetime.datetime.now() # Displays the difference between the target date and the current date
+	if (diff <= timedelta_zero): # Breaks out of the while loop if time delta is less than or equal to 0:00:00
 		break
-	time.sleep(1)
+	if (len(str(diff)) >= int(21)): # Checks if there are still days left
+		identification = int(1) # This is set to detect if there are still days left
+		day, _, time_dec = str(diff).split() # Splits diff into days and time_dec
+		hour_final, minute_final, second = time_dec.split(":") # Splits the time_dec into hours, minutes, and seconds
+		second_final, _ = second.split(".") # Removes microseconds from the second string
+	else:
+		identification = int(2) # This is set to detect if there are only time left
+		hour_final, minute_final, second = str(diff).split(":") # Splits the time_dec into hours, minutes, and seconds
+		second_final, _ = second.split(".") # Removes microseconds from the second string
 
-	if (identifacation == int(1)):
+	if identification == int(1): # If there are days
+		get_year = int(day) / float(365) # This is used to get the year in a decimal format
+		year_final, year_fraction = divmod(get_year, 1) # Does floor division and the modulus operator and crates a tuple
+		day_final = round(year_fraction * 365.25) # Gets the rounded days
 
-		years = int(day_str) / float(365)
-
-		year_final, year_fraction = divmod(years, 1)
-		int_year_final = int(year_final)
-		str_years_final = str(int_year_final)
-
-		rounded_days = round(year_fraction * 365.25)
-		int_days_final = int(rounded_days)
-		str_days_final = str(int_days_final)
-
-		Date0 = [str_years_final]
-		Date1 = [str_days_final]
-		Date2 = [hour_str]
-		Date3 = [minute_str]
-		Date4 = [second_str]
-
-
-	# if (int(int_year_final) != int(0)):
-		if str_years_final <= str("9"):
-			str_years_final = str(str_years_final)
-
-		for str0 in Date0:
-			if (str0 != str(0)):
-				if (str(str_years_final) == str("1")):
-					outline = "-" * 10
-					years = "|" + " " + str_years_final + " " + "Year" + " " + "|"
-
-					print(outline)
-					print(years)
-					print(outline)
-				else:
-					years = "|" + " " + str_years_final + " " + "Years" + " " + "|"
-
-					outline = "-" * len(years)
-
-					print(outline)
-					print(years)
-					print(outline)
-
-	# # if (int(int_days_final) != int(0)):
-	# 	if int_days_final <= int(9):
-	# 		str_days_final = str(str_days_final[1])
-
-		for str1 in Date1:
-			if (str1 != str(0)):
-				if (str(str_days_final) == str("1")):
-					outline = "-" * 9
-					days = "|" + " " + str_days_final + " " + "Day" + " " + "|"
-
-					print(outline)
-					print(days)
-					print(outline)
-				else:
-					days = "|" + " " + str_days_final + " " + "Days" + " " + "|"
-
-					outline = "-" * len(days)
-
-					print(outline)
-					print(days)
-					print(outline)
-
-	# if (int(hour_str) != int(0)):
-		if int(hour_str) <= int(9):
-			hour_str = str(hour_str)
-
-		for str2 in Date2:
-			if (hour_str != int(0)):
-				if (int(hour_str) == int(1)):
-					outline = "-" * 10
-					hours = "|" + " " + hour_str + " " + "Hour" + " " + "|"
-
-					print(outline)
-					print(hours)
-					print(outline)
-				else:
-					hours = "|" + " " + hour_str + " " + "Hours" + " " + "|"
-
-					outline = "-" * len(hours)
-
-					print(outline)
-					print(hours)
-					print(outline)
-
-	# if (int(minute_str) != int(0)):
-		if minute_str <= str("09"):
-			minute_str = str(minute_str[1])
-
-		for str3 in Date3:
-			if (int(minute_str) != int(0)):
-				if (int(minute_str) == int(1)):
-					outline = "-" * 12
-					minutes = "|" + " " + minute_str + " " + "Minute" + " " + "|"
-
-					print(outline)
-					print(minutes)
-					print(outline)
-				else:
-					minutes = "|" + " " + minute_str + " " + "Minutes" + " " + "|"
-					
-					outline = "-" * len(minutes)
-
-					print(outline)
-					print(minutes)
-					print(outline)
-
-
-	# if (int(second_str) != int(0)):
-		if (second_str) <= str("09"):
-			second_str = str(second_str[1])
-
-		for str4 in Date4:
-			if (int(second_str) == int(1)):
-				outline = "-" * 12
-				seconds = "|" + " " + second_str + " " + "Second" + " " + "|"
-
-				print(outline)
-				print(seconds)
-				print(outline)
+		if int(year_final) != int(0): # Checks to make sure that year_final is not equal to 0 before it runs
+			if int(year_final) == int(1): # The code in this if statement will only run if the year_final is equal to 1
+				outline = "-" * 10 # This is for the box around the year
+				years = "| {x} Year |".format(x = int(year_final)) # Stores the data for years
+				print(outline) # Prints the outline
+				print(years) # Prints the years
+				print(outline) # Prints the outline
 			else:
-				seconds = "|" + " " + second_str + " " + "Seconds" + " " + "|"
+				years = "| {x} Years |".format(x = int(year_final)) # Stores the data for years
+				outline = "-" * len(years) # Creates the outline dynamically with the number of characters
+				print(outline) # Prints the outline
+				print(years) # Prints the years
+				print(outline) # Prints the outline
 
-				outline = "-" * len(seconds)
-
-				print(outline)
-				print(seconds)
-				print(outline)
-
-	elif (identifacation == int(2)):
-
-		Date2 = [hour_str]
-		Date3 = [minute_str]
-		Date4 = [second_str]
-
-	# # if (int(hour_str) != int(0)):
-	# 	if hour_str <= str("09"):
-	# 		hour_str = str(hour_str)
-
-		for str2 in Date2:
-			if (int(hour_str) != int(0)):
-				if (int(hour_str) == int(1)):
-					outline = "-" * 10
-					hour = "|" + " " + hour_str + " " + "Hour" + " " + "|"
-
-					print(outline)
-					print(hour)
-					print(outline)
-				else:
-					outline = "-" * 11
-					hours = "|" + " " + hour_str + " " + "Hours" + " " + "|"
-
-					outline = "-" * len(hours)
-
-					print(outline)
-					print(hours)
-					print(outline)
-
-	# if (int(minute_str) != int(0)):
-		if minute_str <= str("09"):
-			minute_str = str(minute_str[1])
-
-		for str3 in Date3:
-			if (int(minute_str) != int(0)):
-				if (int(minute_str) == int(1)):
-					outline = "-" * 12
-					minutes = "|" + " " + minute_str + " " + "Minute" + " " + "|"
-
-					print(outline)
-					print(minutes)
-					print(outline)
-				else:
-					outline = "-" * 14
-					minutes = "|" + " " + minute_str + " " + "Minutes" + " " + "|"
-					
-					outline = "-" * len(minutes)
-
-					print(outline)
-					print(minutes)
-					print(outline)
-
-	# if (int(second_str) != int(0)):
-		if int(second_str) <= int(9):
-			second_str = str(second_str[1])
-
-		for str4 in Date4:
-			if (int(second_str) == int(1)):
-				outline = "-" * 12
-				seconds = "|" + " " + second_str + " " + "Second" + " " + "|"
-
-				print(outline)
-				print(seconds)
-				print(outline)
+		if int(day_final) != int(0): # Checks to make sure that day_final is not equal to 0 before it runs
+			if int(day_final) == int(1): # The code in this if statement will only run if the day_final is equal to 1
+				outline = "-" * 9 # This is for the box around the day
+				days = "| {x} Day |".format(x = int(day_final)) # Stores data for days
+				print(outline) # Prints the outline
+				print(days) # Prints the days
+				print(outline) # Prints the outline
 			else:
-				seconds = "|" + " " + second_str + " " + "Seconds" + " " + "|"
+				days = "| {x} Days |".format(x = int(day_final)) # Stores data for days
+				outline = "-" * len(days) # Creates the outline dynamically with the number of characters
+				print(outline) # Prints the outline
+				print(days) # Prints the days
+				print(outline) # Prints the outline
 
-				outline = "-" * len(seconds)
+		if int(hour_final) != int(0): # Checks to make sure that hour_final is not equal to 0 before it runs
+			if int(hour_final) == int(1): # The code in this if statement will only run if the hour_final is equal to 1
+				outline = "-" * 10 # This is for the box around the hour
+				hours = "| {x} Hour |".format(x = int(hour_final)) # Stores data for hours
+				print(outline) # Prints the outline
+				print(hours) # Prints the hours
+				print(outline) # Prints the outline
+			else:
+				hours = "| {x} Hours |".format(x = int(hour_final)) # Stores data for hours
+				outline = "-" * len(hours) # Creates the outline dynamically with the number of characters
+				print(outline) # Prints the outline
+				print(hours) # Prints the hours
+				print(outline) # Prints the outline
 
-				print(outline)
-				print(seconds)
-				print(outline)
-print("Time is up")
+		if int(minute_final) != int(0): # Checks to make sure that minute_final is not equal to 0 before it runs
+			if int(minute_final) == int(1): # The code in this if statement will only run if the minute_final is equal to 1
+				outline = "-" * 12 # This is for the box around the minute
+				minutes = "| {x} Minute |".format(x = int(minute_final)) # Stores data for minutes
+				print(outline) # Prints the outline
+				print(minutes) # Prints the minutes
+				print(outline) # Prints the outline
+			else:
+				minutes = "| {x} Minutes |".format(x = int(minute_final)) # Stores data for minutes
+				outline = "-" * len(minutes) # Creates the outline dynamically with the number of characters
+				print(outline) # Prints the outline
+				print(minutes) # Prints the minutes
+				print(outline) # Prints the outline
+
+		if int(second_final) == int(1): # Checks to make sure that second_final is not equal to 0 before it runs
+			outline = "-" * 12 # This is for the box around the second
+			seconds = "| {x} Second |".format(x = int(second_final)) # Stores data for seconds
+			print(outline) # Prints the outline
+			print(seconds) # Prints the seconds
+			print(outline) # Prints the outline
+		else:
+			seconds = "| {x} Seconds |".format(x = int(second_final)) # Stores data for seconds
+			outline = "-" * len(seconds) # Creates the outline dynamically with the number of characters
+			print(outline) # Prints the outline
+			print(seconds) # Prints the seconds
+			print(outline) # Prints the outline
+
+	elif identification == int(2): # Sets the identification
+		if int(hour_final) != int(0): # Checks to make sure that hour_final is not equal to 0 before it runs
+			if int(hour_final) == int(1): # The code in this if statement will only run if the hour_final is equal to 1
+				outline = "-" * 10 # This is for the box around the hour
+				hours = "| {x} Hour |".format(x = int(hour_final)) # Stores data for hours
+				print(outline) # Prints the outline
+				print(hours) # Prints the hours
+				print(outline) # Prints the outline
+			else:
+				hours = "| {x} Hours |".format(x = int(hour_final)) # Stores data for hours
+				outline = "-" * len(hours) # Creates the outline dynamically with the number of characters
+				print(outline) # Prints the outline
+				print(hours) # Prints the hours
+				print(outline) # Prints the outline
+
+		if int(minute_final) != int(0): # Checks to make sure that minute_final is not equal to 0 before it runs
+			if int(minute_final) == int(1): # The code in this if statement will only run if the minute_final is equal to 1
+				outline = "-" * 12 # This is for the box around the minute
+				minutes = "| {x} Minute |".format(x = int(minute_final)) # Stores data for minutes
+				print(outline) # Prints the outline
+				print(minutes) # Prints the minute
+				print(outline) # Prints the outline
+			else:
+				minutes = "| {x} Minutes |".format(x = int(minute_final)) # Stores data for minutes
+				outline = "-" * len(minutes) # Creates the outline dynamically with the number of characters
+				print(outline) # Prints the outline
+				print(minutes) # Prints the minutes
+				print(outline) # Prints the outline
+
+		if int(second_final) == int(1): # The code in this if statement will only run if the second_final is equal to 1
+			outline = "-" * 12 # This is for the box around the second
+			seconds = "| {x} Second |".format(x = int(second_final)) # Stores data for seconds
+			print(outline) # Prints the outline
+			print(seconds) # Prints the seconds
+			print(outline) # Prints the outline
+		else:
+			seconds = "| {x} Seconds |".format(x = int(second_final)) # Stores data for seconds
+			outline = "-" * len(seconds) # Creates the outline dynamically with the number of characters
+			print(outline) # Prints the outline
+			print(seconds) # Prints the seconds
+			print(outline) # Prints the outline
+print("Time is up") # Prints when broken out of loop
